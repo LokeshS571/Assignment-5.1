2ea3521f7635d863efbfd2cdf96736b7d2a0bcdd
LokeshS571 <lokeshv542@gmail.com>
2017-03-09 14:46:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var name="Lokesh S";
+var dob= "24th June 1995";
+var age=21;
+var pob="Bengaluru";
+
+console.log("Name :",name);
+console.log("Age :",age);
+console.log("Date of Birth :",dob);
+console.log("place of Birth :",pob);
